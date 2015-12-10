78dc787716a0304644847c0200450d4112f15e0a
Kelebro63 <kelebro63@gmail.com>
2015-12-10 13:40:54 +0300

    inject dependies part1

---
+++ AirNAvigate/app/src/main/java/com/example/airnavigate/Components/AppComponent.java
@@ -0,0 +1,8 @@
+package com.example.airnavigate.Components;
+
+/**
+ * Created by Bistrov Alexey on 10.12.2015.
+ */
+public interface AppComponent {
+
+}
--- AirNAvigate/app/src/main/java/com/example/airnavigate/MyApplication.java
+++ AirNAvigate/app/src/main/java/com/example/airnavigate/MyApplication.java
@@ -3,17 +3,33 @@ package com.example.airnavigate;
 import android.app.Application;
 import android.content.Context;
 
+import com.example.airnavigate.Components.AppComponent;
+
 /**
  * Created by Kelebro63 on 29.11.2015.
  */
 public class MyApplication extends Application {
     private static MyApplication instance;
+    private AppComponent appComponent;
 
     public MyApplication() {
         instance = this;
     }
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initAppComponent();
+    }
+
+    private void initAppComponent() {
+        appComponent = DaggerAppComponent.builder()
+                .appModule(new AppModule(this))
+                .build();
+    }
+
     public static Context getContext() {
         return instance;
     }
+
 }
